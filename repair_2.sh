3054bca3d2127b6b0b127e2c2db7b242a68e319a
jaelynbos <jbos@ucsc.edu>
2026-03-18 17:20:36 -0700

    fixed typo

--- repair_2.sh
+++ repair_2.sh
@@ -6,14 +6,16 @@
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=180G
-#SBATCH --partition=1024x28
+#SBATCH --partition=lab-mpinsky
+#SBATCH --account=pi-mpinsky
+#SBATCH --qos=pi-mpinsky
 #SBATCH --time=24:00:00
 
-#Required software: bbtools
+module load bbtools
 
-for file1 in $(ls -v ../../scratch/jbos/trim2/*.fp2_r1.fq.gz)
+for file1 in $(ls -v /scratch/jbos/trim2/*.fp2_r1.fq.gz)
 do
 	name1=$(basename "$file1")
 	name1="${name1%.fp2_r1.fq.gz}"
-	repair.sh in1=../../scratch/jbos/trim2/${name1}.fp2_r1.fq.gz in2=../../scratch/jbos/trim2/${name1}.fp2_r2.fq.gz out1=repaired2/${name1}.fp2_r1.fq.gz out2=repaired2/${name1}.fp2_r2.fq.gz outs=singletons2/${name1}.fq repair
+	repair.sh in1=/scratch/jbos/trim2/${name1}.fp2_r1.fq.gz in2=/scratch/jbos/trim2/${name1}.fp2_r2.fq.gz out1=repaired2/${name1}.fp2_r1.fq.gz out2=repaired2/${name1}.fp2_r2.fq.gz outs=singletons2/${name1}.fq repair
 done
