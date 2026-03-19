3815254be1ff3e9727b92d2595224bbdc88ca168
jaelynbos <jbos@ucsc.edu>
2026-03-18 18:02:11 -0700

    added Cladocopium

--- samtools_index_loop.sh
+++ samtools_index_loop.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 #SBATCH --job-name=samtools
-#SBATCH -o samtools-%A_%a.out
-#SBATCH --partition=256x44
+#SBATCH -o samtools-%j.out
+#SBATCH --partition=lab-mpinsky
+#SBATCH --account=pi-mpinsky
+#SBATCH --qos=pi-mpinsky
 #SBATCH --cpus-per-task=16
 #SBATCH --mail-user=jbos@ucsc.edu
 #SBATCH --mail-type=ALL
 #SBATCH --mem=180G
 #SBATCH --time=12:00:00
 
-for file in $(ls -v ../../scratch/jbos/bamfiles_sorted/*.bam)
+for file in $(ls -v /scratch/jbos/cladocopium_bam_sorted/*.bam)
 
 do
     sample=$(basename "$file" | cut -d. -f1)
-	samtools index -b ../../scratch/jbos/bamfiles_sorted/$sample.bam ../../scratch/jbos/bamfiles_sorted/$sample.bai
+	samtools index -b /scratch/jbos/cladocopium_bam_sorted/$sample.bam /scratch/jbos/cladocopium_bam_sorted/$sample.bai
+done
+
+for file in $(ls -v /scratch/jbos/bam_sorted/*.bam)
+
+do
+    sample=$(basename "$file" | cut -d. -f1)
+	samtools index -b /scratch/jbos/bam_sorted/$sample.bam /scratch/jbos/bam_sorted/$sample.bai
 done
